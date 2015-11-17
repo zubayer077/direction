b692746911b7ac78c8b9a24af160d9f3adfcbeb3
Hossan Zubayer <hossan.zubayer@exactEarth.com>
2015-11-17 13:45:14 -0500

    is it faster this way, hard to decide

--- server/api/email.js
+++ server/api/email.js
@@ -4,43 +4,43 @@ var nodemailer = require('nodemailer'),
 // create reusable transporter object using SMTP transport
 var transporter =  new Array();
 
-transporter[0] = {};
-transporter[0].from = 'h.zu@aol.com';
-transporter[0].smtp  = nodemailer.createTransport({
-    domains: ["aol.com"],
-    host: "smtp.aol.com",
-    port: 587,
-    auth: {
-        user: 'h.zu@aol.com',
-        pass: 'Viking724'
-    }
+transporter.push({
+	from : 'hmz820@mun.ca',
+	smtp : nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'hmz820@mun.ca',
+			pass: 'Viking0615'
+		}
+	});
 });
 
-
-transporter[1] = {};
-transporter[1].from = 'hmz820@mun.ca';
-transporter[1].smtp = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'hmz820@mun.ca',
-        pass: 'Viking0615'
-    }
+transporter.push({
+	from : 'h.zu@aol.com',
+	smtp : nodemailer.createTransport({
+		domains: ["aol.com"],
+		host: "smtp.aol.com",
+		port: 587,
+		auth: {
+			user: 'h.zu@aol.com',
+			pass: 'Viking724'
+		}
+	});
 });
 
-
-transporter[2] = {};
-transporter[2].from = 'i@zubayer.com';
-transporter[2].smtp = nodemailer.createTransport(smtpTransport({
-    host: 'rsb20.rhostbh.com',
-    port: 465,
-        secure: true,
-        authMethod: 'PLAIN',
-    auth: {
-        user: 'i@zubayer.com',
-        pass: 'Viking724'
-    }
-}));
-
+transporter.push({ 
+	from : 'i@zubayer.com',
+	smtp : nodemailer.createTransport(smtpTransport({
+		host: 'rsb20.rhostbh.com',
+		port: 465,
+			secure: true,
+			authMethod: 'PLAIN',
+		auth: {
+			user: 'i@zubayer.com',
+			pass: 'Viking724'
+		}
+	}));
+});
 
 
 var _self = function (content, res) {
